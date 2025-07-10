bc9aca2fccc82089a626288875014ea95a79dfed
Lalit0101 <lalitkp0101@gmail.com>
2025-07-10 15:31:10 +0530

    new done the

--- student/urls.py
+++ student/urls.py
@@ -23,5 +23,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('student_app.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ 
